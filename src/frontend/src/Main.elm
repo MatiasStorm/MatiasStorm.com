4489415b3b2df26458427d9b7b1f5abaa5eee822
MatiasStorm <mat-435@hotmail.com>
2020-10-16 16:25:20 +0200

    remove elm-stuff

--- src/frontend/src/Main.elm
+++ src/frontend/src/Main.elm
@@ -1,24 +1,69 @@
 module Main exposing (..)
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, Attribute, button, div, text, input)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+  Browser.sandbox { init = init, update = update, view = view }
 
-type Msg = Increment | Decrement
+type Msg = 
+    Increment 
+    | Decrement 
+    | Reset
+    | ChangeText String
 
+type alias Model = 
+    {
+        counter : Int,
+        text: String
+    }
+
+init : Model
+init = 
+    {
+        counter = 0,
+        text = ""
+    }
+
+update : Msg -> Model -> Model
 update msg model =
   case msg of
     Increment ->
-      model + 1
+        {model | counter = model.counter + 1}
 
     Decrement ->
-      model - 1
+        {model | counter = model.counter + 1}
 
+    Reset ->
+        {model | counter = 0}
+
+    ChangeText newContent ->
+        {model | text = newContent}
+
+
+view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
+  div [] 
+    [ 
+        counterView model
+        , div [] 
+        [
+            input [ placeholder "Text to reverse", value model.text, onInput ChangeText] []
+            , div [] [text ( String.reverse model.text )]
+        ]
     ]
+
+counterView : Model -> Html Msg
+counterView model  =
+    div []
+    [
+        button [ onClick Decrement ] [ text "-" ]
+        , div [] 
+        [ 
+            text (String.fromInt model.counter)
+            , button [onClick Reset ] [text "Reset"]
+        ]
+        , button [ onClick Increment ] [ text "+" ]
+    ]
+
