1c905c621362ee78ba499f18f71c615b553708ae
MatiasStorm <mat-435@hotmail.com>
2020-10-28 17:14:09 +0100

    Major refactor

--- src/frontend/src/Page/Admin.elm
+++ src/frontend/src/Page/Admin.elm
@@ -9,6 +9,7 @@ type Msg
     = GotPosts (Result Http.Error (List Post))
     | GotCategories (Result Http.Error (List PostCategory))
     | ChangeShowNewPost Bool
+    | SetEditingPost (Maybe Post)
 
 
 -- Model
@@ -21,17 +22,25 @@ type Login
     = Authorized
     | Unauthorized
 
+type alias NewPost = 
+    { title: String
+    , text: String
+    , categories : (List String)
+    , serie : Maybe String
+    , published : Bool
+    }
+
 type alias Model =
     { posts : (List Post)
     , postCategories : (List PostCategory)
     , status : Status
     , login : Login
     , showNewPost : Bool
-    , newPost : Post
+    , newPost : NewPost
+    , editingPost : Maybe Post
     }
 
 -- Update
-
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     let 
@@ -61,20 +70,20 @@ update msg model =
                     ({model | status = Failure}, Cmd.none)
 
         ChangeShowNewPost bool ->
-            ({model | showNewPost = bool}, Cmd.none)
+            ({model | showNewPost = bool, editingPost = Nothing}, Cmd.none)
+
+        SetEditingPost post ->
+            ({model | editingPost = post, showNewPost = False}, Cmd.none)
 
 
 -- Init
-initialNewPost : Post
+initialNewPost : NewPost
 initialNewPost = 
-    { id = Nothing
-    , title = ""
+    { title = ""
     , text = ""
     , categories = [""]
     , published = False
     , serie = Nothing
-    , created = Nothing
-    , updated = Nothing
     } 
 
 initialModel : Model
@@ -85,6 +94,7 @@ initialModel =
     , login = Unauthorized
     , showNewPost = False
     , newPost = initialNewPost
+    , editingPost = Nothing
     }
 
 
@@ -92,35 +102,85 @@ init : (Model, Cmd Msg)
 init =
     (initialModel, Cmd.batch [getBlogCategories GotCategories, getBlogPosts GotPosts ]  )
 
+htmlIf : (Bool, Html msg) -> Html msg
+htmlIf (bool, component) =
+    case bool of
+        True ->
+            component
+
+        False ->
+            text ""
+
 view : Model -> Html Msg
 view model =
     let
         optionView : PostCategory -> Html msg
         optionView category = option [ Attr.id "postCategories" ] [text category.category_name] 
 
-        htmlIf : (Bool, Html msg) -> Html msg
-        htmlIf (bool, component) =
-            case bool of
-                True ->
-                    component
-
-                False ->
-                    text ""
     in
     div [ Attr.class "container-fluid" ] 
-        [ htmlIf 
-            ( not model.showNewPost
-            , button 
-                [ Attr.class "btn btn-success"
-                , onClick (ChangeShowNewPost True)
-                ] [ text "New Post" ])
-        , htmlIf (model.showNewPost, editPostView {model = model, post = Nothing, cancelMsg = ( ChangeShowNewPost False )})
-        , h2 [] [ text "Blog Posts:" ]
+        [ newPostButtonView model
+        , editNewPostView model
         , postTableView model
+        , editExistingPostView model
         ]
 
-editPostView : {model: Model, post: Maybe Post, cancelMsg: Msg} -> Html Msg
-editPostView {model, post, cancelMsg} =
+newPostButtonView : Model -> Html Msg
+newPostButtonView model = 
+    case model.editingPost of
+        Just _ ->
+            text ""
+
+        Nothing ->
+            htmlIf 
+                ( not model.showNewPost
+                , button 
+                    [ Attr.class "btn btn-success"
+                    , onClick (ChangeShowNewPost True)
+                    ] [ text "New Post" ])
+
+editNewPostView : Model -> Html Msg
+editNewPostView model =
+    htmlIf 
+        (model.showNewPost
+        , editPostView 
+            {model = model
+            , post = Nothing
+            , cancelMsg = ( ChangeShowNewPost False )
+            }
+        )
+
+editExistingPostView : Model -> Html Msg
+editExistingPostView model =
+    let 
+        renderEdit : Bool
+        renderEdit =
+            case model.editingPost of
+                Just actualEditingPost ->
+                    True
+
+                Nothing ->
+                    False
+    in
+    htmlIf 
+        ( renderEdit 
+        , editPostView 
+            {model = model
+            , post = model.editingPost
+            , cancelMsg = ( SetEditingPost Nothing )
+            }
+        )
+
+editPostView : 
+    { model: Model
+    , post: Maybe Post
+    , cancelMsg: Msg
+    } -> Html Msg
+editPostView 
+    { model
+    , post
+    , cancelMsg
+    } =
     let
         optionView : PostCategory -> Html msg
         optionView category = option [ Attr.id "postCategories" ] [text category.category_name] 
@@ -156,16 +216,26 @@ editPostView {model, post, cancelMsg} =
         , button [ Attr.class "btn btn-secondary", onClick cancelMsg ] [ text "Cancel" ]
         ]
 
-postTableView : Model -> Html msg
+postTableView : Model -> Html Msg
 postTableView model =
     let
-        postRow : Post -> Html msg
+        postRow : Post -> Html Msg
         postRow post =
             div [Attr.class "row border-bottom py-2 border-grey justify-content-between"] 
                 [ h5 [ Attr.class "col-auto" ] [ text post.title ]  
-                , button [ Attr.class "col-auto btn btn-sm btn-secondary" ] [ text "Edit" ]
+                , button [ Attr.class "col-auto btn btn-sm btn-secondary", onClick (SetEditingPost (Just post)) ] [ text "Edit" ]
                 ]
     in
-    div [ Attr.class "col-6" ] 
-        (List.map postRow model.posts)
+    case model.editingPost of
+        Just _ ->
+            text ""
+        
+        Nothing ->
+            htmlIf 
+                ( not model.showNewPost 
+                , div [ Attr.class "col-6" ] 
+                    [ h2 [] [ text "Blog Posts:" ]
+                    , div [] (List.map postRow model.posts)
+                    ]
+                )
 
