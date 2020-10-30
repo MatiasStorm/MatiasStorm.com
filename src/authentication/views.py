86518482de671ebc501483f09062de55704393d5
MatiasStorm <mat-435@hotmail.com>
2020-10-30 20:59:00 +0100

    change refreash_token to refresh

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -10,7 +10,7 @@ class LogoutAndBlacklistRefreshTokenForUserView(APIView):
 
     def post(self, request):
         try:
-            refresh_token = request.data["refresh_token"]
+            refresh_token = request.data["refresh"]
             token = RefreshToken(refresh_token)
             token.blacklist()
             return Response(status=status.HTTP_205_RESET_CONTENT)
